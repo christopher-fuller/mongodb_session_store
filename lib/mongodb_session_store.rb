5aa06a761897bb35c848ce3f1a96ad2898f84ef3
Christopher Fuller <git@chrisfuller.me>
2012-01-17 17:41:09 -0800

    Added Railtie class

--- lib/mongodb_session_store.rb
+++ lib/mongodb_session_store.rb
@@ -1,4 +1,9 @@
 require 'mongodb_session_store/mongodb_session_store'
 
 module MongodbSessionStore
+  class Railtie < Rails::Railtie
+    rake_tasks do
+      load 'tasks/mongodb_session_store_tasks.rake'
+    end
+  end
 end
